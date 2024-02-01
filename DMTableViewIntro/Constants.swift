450d0cce0996948ab5a6915786c59238b366fc60
Beau Enslow <beauenslow@Beaus-MacBook-Pro.local>
2024-02-01 12:02:35 -0700

    Finished

---
+++ DMTableViewIntro/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  DMTableViewIntro
+//
+//  Created by Beau Enslow on 2/1/24.
+//
+
+import Foundation
+
+
+struct K {
+    static let cellIdentifier = "ReuseCell"
+}
--- DMTableViewIntro/ExpensesViewController.swift
+++ DMTableViewIntro/ExpensesViewController.swift
@@ -9,14 +9,29 @@ import UIKit
 
 class ExpensesViewController: UIViewController {
     
+    @IBOutlet weak var tableView: UITableView!
     let expenses = ["Rent", "Groceries", "Utilities", "Transportation", "Entertainment", "Clothing", "Eating Out", "Travel", "Insurance", "Phone Bill"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        tableView.dataSource = self
     }
     
     func configureTableView() {
         
     }
+}
+
+extension ExpensesViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return expenses.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
+        cell.textLabel?.text = expenses[indexPath.row]
+        return cell
+    }
+    
+    
 }
